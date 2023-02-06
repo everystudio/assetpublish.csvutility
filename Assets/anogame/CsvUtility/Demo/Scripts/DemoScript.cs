5047f8959e1e053d31a0f74cf506c6747d55d525
watanabe@marida <watanabe.everystudio@gmail.com>
2023-02-06 13:23:47 +0900

    プロジェクト追加

---
+++ Assets/anogame/CsvUtility/Demo/Scripts/DemoScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoScript : MonoBehaviour
+{
+    [SerializeField] private TextAsset localCsvFile;
+
+    [SerializeField] private string saveFilename = "test_file";
+
+    public void LoadFromLocalCsvFile()
+    {
+        if (localCsvFile != null)
+        {
+            SampleModel sampleModel = new SampleModel();
+            if (sampleModel.Load(localCsvFile))
+            {
+                sampleModel.CheckDebugLog();
+            }
+        }
+        else
+        {
+            Debug.LogWarning("not set csv file!!");
+        }
+    }
+
+    public void SaveToPersistentDataPath()
+    {
+        SampleModel sampleModel = new SampleModel();
+        sampleModel.List.Add(new SampleModelParam() { test_int = 10, test_float = 10.0f });
+        sampleModel.List.Add(new SampleModelParam() { test_int = 12, test_float = 12.0f });
+
+        sampleModel.Save(saveFilename);
+        Debug.Log("Check your directroy");
+        Debug.Log($"{System.IO.Path.Combine(Application.persistentDataPath, saveFilename) }");
+    }
+
+    public void LoadFromPersistentDataPath()
+    {
+        SampleModel sampleModel = new SampleModel();
+        if (sampleModel.Load(saveFilename))
+        {
+            sampleModel.CheckDebugLog();
+        }
+        else
+        {
+            Debug.LogError("not found saved csv file");
+            Debug.LogError($"{System.IO.Path.Combine(Application.persistentDataPath, saveFilename) }");
+        }
+    }
+
+}
---
+++ Assets/anogame/CsvUtility/Demo/Scripts/SampleModel.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using anogamelib;
+
+public class SampleModelParam : CsvModelParam
+{
+    public int test_int;
+    public float test_float;
+
+    public override string ToString()
+    {
+        return $"test_int={test_int} test_float={test_float}";
+    }
+}
+
+public class SampleModel : CsvModel<SampleModelParam>
+{
+}
